39df684bbecf214d89fc94f003af4a7abdfb079a
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-09-17 20:17:58 +0530

    first commit

---
+++ src/test/java/com/springData/learn_spring/data/jpa/LearnSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springData.learn_spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
